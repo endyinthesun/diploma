5cf9f6e9bc6d40c5a449e73b93e26eba48836913
endy <vlad.kalenikov@gmail.com>
2020-12-21 22:34:03 +0200

    add basic style and bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss'
+// import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   // <React.StrictMode>
